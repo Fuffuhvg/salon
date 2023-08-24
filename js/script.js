e8ab296fdfa929aa7f2f9f9dc7270a82fd65795a
kakashi's left eye <135690017+Fuffuhvg@users.noreply.github.com>
2023-08-24 21:58:20 +0300

    swiper css js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+  
    //scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -29,3 +30,38 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+  
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+
+        520: {
+            slidesPerView: 2,
+        },
+
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  }); 
+   
+   
+
